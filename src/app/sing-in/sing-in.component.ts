309cd36cf717789e8ff84a08bef386b1b6b89902
youssefoume <youssef.net57@gmail.com>
2023-03-17 11:23:40 +0100

    updating

--- src/app/sing-in/sing-in.component.ts
+++ src/app/sing-in/sing-in.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from '../services/auth.service';
 
 
 @Component({
@@ -12,32 +11,31 @@ export class SingInComponent {
   email : string = '';
   password : string = '';
 
-  constructor(private auth : AuthService) { }
 
   ngOnInit(): void {
   }
 
   login() {
 
-    if(this.email == '') {
-      alert('Please enter email');
-      return;
-    }
+    // if(this.email == '') {
+    //   alert('Please enter email');
+    //   return;
+    // }
 
-    if(this.password == '') {
-      alert('Please enter password');
-      return;
-    }
+    // if(this.password == '') {
+    //   alert('Please enter password');
+    //   return;
+    // }
 
-    this.auth.login(this.email,this.password);
+    // this.auth.login(this.email,this.password);
     
-    this.email = '';
-    this.password = '';
+    // this.email = '';
+    // this.password = '';
 
   }
 
   signInWithGoogle() {
-    this.auth.googleSignIn();
+    // this.auth.googleSignIn();
   }
 
 }
