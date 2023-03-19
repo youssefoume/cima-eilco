b6fc20d817c34b9c6128c1da40aca94945a12910
youssefoume <youssef.net57@gmail.com>
2023-03-19 02:55:20 +0100

    added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,14 @@
 
 export const environment = {
   production: false,
-  firebase : {
-    apiKey: "AIzaSyDbhdWawp0lmRKtCzg_0jmA_dy_pk2gKfg",
-    authDomain: "hotel-management-3390d.firebaseapp.com",
-    projectId: "hotel-management-3390d",
-    storageBucket: "hotel-management-3390d.appspot.com",
-    messagingSenderId: "1010076842792",
-    appId: "1:1010076842792:web:a1f076a7eb815a0aae6e1d"
+  firebase: {
+    projectId: 'cima-eilco-36267',
+    appId: '1:164050656672:web:e99280c1cedec8371a98c5',
+    databaseURL: 'https://cima-eilco-36267-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'cima-eilco-36267.appspot.com',
+    apiKey: 'AIzaSyC-jEH6GPs6QkX0LEW0uZg2WvKj7SZNscg',
+    authDomain: 'cima-eilco-36267.firebaseapp.com',
+    messagingSenderId: '164050656672',
   }
 };
 
