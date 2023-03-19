0d5f594f4b69121837eb033f38b53fc62f97f4e5
youssefoume <youssef.net57@gmail.com>
2023-03-19 03:23:51 +0100

    finally

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,15 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'cima-eilco-36267',
-    appId: '1:164050656672:web:e99280c1cedec8371a98c5',
-    databaseURL: 'https://cima-eilco-36267-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'cima-eilco-36267.appspot.com',
-    apiKey: 'AIzaSyC-jEH6GPs6QkX0LEW0uZg2WvKj7SZNscg',
-    authDomain: 'cima-eilco-36267.firebaseapp.com',
-    messagingSenderId: '164050656672',
+
+      apiKey: "AIzaSyATtfwiJQX_cSl40wP4v0FSW5pj8iuykZI",
+      authDomain: "cima-eilco-33e3f.firebaseapp.com",
+      projectId: "cima-eilco-33e3f",
+      storageBucket: "cima-eilco-33e3f.appspot.com",
+      messagingSenderId: "647898773250",
+      appId: "1:647898773250:web:fb671e7229db787132f41b",
+    databaseURL: 'https://cima-eilco-33e3f-default-rtdb.europe-west1.firebasedatabase.app',
+   
   }
 };
 
