4ab35d9c6f412c70219276894b4565b3c17640ac
youssefoume <youssef.net57@gmail.com>
2023-03-19 12:54:14 +0100

    add

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/firebase-angular-crud'),
+      dir: require('path').join(__dirname, './coverage/cima-eilco'),
       subdir: '.',
       reporters: [
         { type: 'html' },
